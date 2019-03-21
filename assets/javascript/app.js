1c982d7127fe01db4cca123815b6f2429945d7f0
Greg Glinoga <gglinoga@gmail.com>
2019-03-20 22:27:43 -0700

    update wed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,93 +3,202 @@ $(document).ready(function () {
         numRight: 0,
         numWrong: 0,
         numUnA: 0,
-        currentQ: "",
-        currentA: "",
+        currentA: 4,
         buttonsExist: 0,
+        i: 0,
         questions: [{
-                q: "question1 goes here",
-                c: ["sample choice one", "sample choice two", "sample choice three", "sample choice four"],
+                q: "Hodor is..?",
+                c: ["alive", "dead", "still holding the door", "at the Wall"],
                 a: 1
             },
             {
-                q: "question2 goes here",
-                c: ["sample choice one", "sample choice two", "sample choice three", "sample choice four"],
+                q: "The seat of the Lannister's power is in..?",
+                c: ["Highgarden", "Summerfell", "Casterly Rock", "Winterfell"],
+                a: 2
+            },
+            {
+                q: "Winter is..?",
+                c: ["coming", "going", "cold", "ice"],
+                a: 0
+            },
+            {
+                q: "Which dragon did the Night King turn?",
+                c: ["Drogon", "Raeghal", "Viserion", "Puff"],
+                a: 2
+            },
+            {
+                q: "Valar Morghulis means..?",
+                c: ["praise the old gods", "don't catch your eggs before they hatch", "always pay your debts", "all men must die"],
+                a: 3
+            },
+            {
+                q: "The Stark sigil bears this animal:",
+                c: ["direwolf", "lion", "fish", "bear"],
+                a: 0
+            },
+            {
+                q: "Along with Valyrian steel, this can slay a white walker...",
+                c: ["sunlight", "dragon glass", "harsh language", "fire"],
                 a: 1
             },
             {
-                q: "question3 goes here",
-                c: ["sample choice one", "sample choice two", "sample choice three", "sample choice four"],
-                a: 1
-            },
-            {
-                q: "question4 goes here",
-                c: ["sample choice one", "sample choice two", "sample choice three", "sample choice four"],
-                a: 1
-            },
-            {
-                q: "question5 goes here",
-                c: ["sample choice one", "sample choice two", "sample choice three", "sample choice four"],
-                a: 1
-            },
-            {
-                q: "question6 goes here",
-                c: ["sample choice one", "sample choice two", "sample choice three", "sample choice four"],
-                a: 1
-            },
-            {
-                q: "question7 goes here",
-                c: ["sample choice one", "sample choice two", "sample choice three", "sample choice four"],
-                a: 1
-            },
-            {
-                q: "question8 goes here",
-                c: ["sample choice one", "sample choice two", "sample choice three", "sample choice four"],
-                a: 1
+                q: "What is the punishment for deserting the Night's Watch?",
+                c: ["lose a hand", "prison", "exile", "death"],
+                a: 3
             },
 
         ],
 
         startup: function () {
+            $("#rightH").remove();
+            $("#right").remove();
+            $("#wrongH").remove();
+            $("#wrong").remove();
             $("#startbutton").remove();
             numRight = 0;
             numWrong = 0;
             numUnA = 0;
-            currentQ = "update to 1st Q";
-            currentA = "update to 1st A";
             if (got.buttonsExist === 0) {
-                var timer = $("<div id='timer' class='card-body'</div>");
-                $("#anchor").append(timer);
-                $("#timer").html("00:00");
-                var question = $("<div id='question' class='card-body'</div>");
-                $("#timer").append(question);
+                var question = $("<div id='question' class='card-body'></div>");
+                $("#anchor").append(question);
                 $("#question").html("1st Question");
-                var choice1 = $("<br> <div id='choice1' class='btn btn-primary btn-lg'>choice1</div>");
-                $("#question").append(choice1);
-                var choice2 = $("<br> <div id='choice2' class='btn btn-primary btn-lg'>choice1</div>");
-                $("#question").append(choice2);
-                var choice3 = $("<br> <div id='choice3' class='btn btn-primary btn-lg'>choice1</div>");
-                $("#question").append(choice3);
-                var choice4 = $("<br> <div id='choice4' class='btn btn-primary btn-lg'>choice1</div>");
-                $("#question").append(choice4);
-                
-                // $("#choice1").text(choice1);
-                ////make buttons/divs
+                var c0 = $("<br> <div id='choice0' class='btn btn-primary btn-lg'>choice0</div>");
+                $("#anchor").append(c0);
+                var c1 = $("<br> <div id='choice1' class='btn btn-primary btn-lg'>choice1</div>");
+                $("#anchor").append(c1);
+                var c2 = $("<br> <div id='choice2' class='btn btn-primary btn-lg'>choice2</div>");
+                $("#anchor").append(c2);
+                var c3 = $("<br> <div id='choice3' class='btn btn-primary btn-lg'>choice3</div>");
+                $("#anchor").append(c3);
+                console.log("end startup")
+                got.game();
                 buttonsExist = 1;
             }
         },
 
-        // qUpdate: function () {
-        //     //timers go in here
-        //     for (var i = 0; i < questions.length; i++) {
+        game: function () {
+            $("#question").html(this.questions[this.i].q);
+            $("#choice0").html(this.questions[this.i].c[0]);
+            $("#choice1").html(this.questions[this.i].c[1]);
+            $("#choice2").html(this.questions[this.i].c[2]);
+            $("#choice3").html(this.questions[this.i].c[3]);
 
-        //     }
-        // }
+            var time = 30;
+            var clockRunning = true;
+            console.log("end game")
+            if (!clockRunning) {
+                console.log("start time!")
+                intervalId = setInterval(count, 1000);
+                clockRunning = true;
+            }
+
+            function count() {
+                time--;
+                console.log(time)
+                $("#timer").html(time);
+                if (time <= 0) {
+                    console.log("time is up")
+                    stop();
+                    clockreset();
+                    got.timeover();
+                    // got.timeover()
+                    // } else if (this.currentA == this.questions[this.i].a) {
+                    //     alert("correct!");
+                }
+            }
+
+            function clockreset() {
+                time = 30;
+                $("#timer").html(time);
+            }
+
+            function stop() {
+                clearInterval(intervalId);
+                clockRunning = false;
+            }
+
+
+        },
+
+
+        ///timer in here, 30s
+        // update q, update a, update choices
+        //end for
+        //if timer runs out, incorrect, delay 5s
+        //if choice = a, correct, delay 5s
+        //if choice != a, incorrect, delay 5s
+        button: function (z) {
+            this.currentA = z;
+            console.log(this.currentA);
+            if (this.currentA === this.questions[this.i].a) {
+                alert("correct")
+                this.numRight++;
+            } else {
+                alert("incorrect")
+                this.numWrong++;
+            }
+            this.i++;
+            if (this.i<8){
+            got.game();
+            }
+            else {
+                alert("Number right = " + this.numRight + "    Number wrong = " + this.numWrong)
+            }
+        },
+
+
+
+        correct: function () {
+
+        },
+
+        incorrect: function () {
+
+        },
+
+        timeover: function () {
+            // alert("time is over");
+            this.numUnA++;
+            console.log("unaswered = " + this.numUnA);
+            this.game();
+        },
+
+        gameover: function () {
+            $("#question").remove();
+            $("#choice0").remove();
+            $("#choice1").remove();
+            $("#choice2").remove();
+            $("#choice3").remove();
+            var rightH = $("<div id='rightH'class='card-header'>Correct:</div>");
+            $("anchor").append(rightH)
+            var right = $("<div id='right'class='card-body'></div>");
+            $("#anchor").append(right);
+            $("#right").html(this.numRight);
+            var wrongH = $("<div id='wrongH'class='card-header'>Incorrect:</div>");
+            $("anchor").append(wrongH)
+            var wrong = $("<div id='wrong' class='card-body'></div>");
+            $("#anchor").append(wrong);
+            $("#wrong").html(this.numWrong);
+
+            this.startup();
+        }
 
     } ///end of got object
-
-    ///start button
     $("#startbutton").on("click", function () {
         got.startup();
+    })
+
+    $("body").on("click", "div#choice0", function () {
+        got.button(0);
+    })
+    $("body").on("click", "div#choice1", function () {
+        got.button(1);
+    })
+    $("body").on("click", "div#choice2", function () {
+        got.button(2);
+    })
+    $("body").on("click", "div#choice3", function () {
+        got.button(3);
     })
 
 }) /////// ready wrapper
